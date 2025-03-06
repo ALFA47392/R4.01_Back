23f9ff77b7cd433a7f9501158038d4ab2eb9c414
ALFA47392 <153225520+ALFA47392@users.noreply.github.com>
2025-03-06 12:05:55 +0100

    gitignore

--- conf.php
+++ conf.php
@@ -2,7 +2,7 @@
 
 // D�finition des informations n�cessaires � la connexion � la base de donn�es.
 // Adresse du serveur MySQL et du port (3306)
-$server = '127.0.0.1:3306';
+$server = '127.0.0.1:8080';
 
 // Nom de la base de donn�es � laquelle on souhaite se connecter
 $db = 'projetr401';
